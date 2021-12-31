b5b03df381cd7b75194b2dcacc45e5fc26d3ea55
k971119 <95323665+HanyeolKo@users.noreply.github.com>
2021-12-31 09:06:05 +0900

    setting-fix : junit 의존성 관련 스코프 수정

--- src/test/java/com/tissue_cell/controller/MyBaitsTest.java
+++ src/test/java/com/tissue_cell/controller/MyBaitsTest.java
@@ -1,13 +1,13 @@
 package com.tissue_cell.controller;
 
 import javax.inject.Inject;
+
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
