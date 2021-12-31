ecebb009f40a33a9716cd845b5897b1540eb4a06
k971119 <95323665+HanyeolKo@users.noreply.github.com>
2021-12-31 09:48:23 +0900

    setting-fix : jenkins 테스트를 위한 jdbc 설정 변경

--- src/test/java/com/tissue_cell/controller/MySQLConnection.java
+++ src/test/java/com/tissue_cell/controller/MySQLConnection.java
@@ -9,7 +9,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/**/root-context.xml" })
