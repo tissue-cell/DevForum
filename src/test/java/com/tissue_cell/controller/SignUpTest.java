119638e56c5ac80f117cfc07fdf5dae874c7bd63
seungji <kwag57@naver.com>
2022-01-18 22:12:41 +0900

    test : 테스트 코드 작성시 오류 해결
    root-context 또는 application-context는 view와 관련되지 않은 객체를 정의한다.
    
    servlet-context.xml 파일은 서블릿 요청과 관련된 객체를 정의해야한다.
    주로 View 관련된, Controller 같은 bean을 설정한다

--- src/test/java/com/tissue_cell/controller/SignUpTest.java
+++ src/test/java/com/tissue_cell/controller/SignUpTest.java
@@ -3,11 +3,21 @@ package com.tissue_cell.controller;
 import static org.junit.Assert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import org.junit.jupiter.api.Test;
+
+import org.junit.Ignore;
+//import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
+import com.fasterxml.jackson.annotation.JacksonInject;
+import com.tissue_cell.dao.LoginDAO;
+import com.tissue_cell.dao.LoginDAOImpl;
 import com.tissue_cell.dto.UserDTO;
 import com.tissue_cell.service.LoginService;
 import com.tissue_cell.service.LoginServiceImpl;
@@ -16,21 +26,30 @@ import com.tissue_cell.service.LoginServiceImpl;
 @ContextConfiguration(locations={"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"})
 public class SignUpTest {
 	
-	private LoginService login = new LoginServiceImpl();
-
+	
+	@Autowired
+	private LoginService login;
+	
 	@Test
+//	@Ignore
 	public void 회원가입() {
 		UserDTO user = new UserDTO();
 		user.setName("test");
 		user.setEmail("test@gmail.com");
 		user.setId("user");
 		user.setPassword("1234");
+		System.out.println("회원가입");
+		if(login.SelectDuplication(user.getId()) > 0) {
+			System.out.println("중복아이디 있음 저장 실패");
+		}else {
+			System.out.println("중복아이디 없음 저장 성공");
+			login.InsertAccount(user);
+		}
 		
-		login.InsertAccount(user);
 		
-		System.out.println(login.SelectDuplication(user.getId()) > 0? "저장완료":"저장 실패");
+		
 	}
-	
+	@Ignore
 	@Test
 	public void 중복회원_체크() {
 		UserDTO user1 = new UserDTO();
@@ -44,9 +63,7 @@ public class SignUpTest {
 		user2.setEmail("test2@gmail.com");
 		user2.setId("user");
 		user2.setPassword("12345");
-		
 		login.InsertAccount(user1);
-		
 		IllegalStateException e = assertThrows(IllegalStateException.class, () -> login.InsertAccount(user2));
 		System.out.println(e.getMessage());
 	}
