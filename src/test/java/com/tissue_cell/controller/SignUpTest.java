f6b5304c718e23557ffb3698c0a41d4da1b9fa1d
k971119 <95323665+HanyeolKo@users.noreply.github.com>
2022-01-19 08:49:27 +0900

    test : 중복 체크 삭제

--- src/test/java/com/tissue_cell/controller/SignUpTest.java
+++ src/test/java/com/tissue_cell/controller/SignUpTest.java
@@ -1,26 +1,17 @@
 package com.tissue_cell.controller;
 
-import static org.junit.Assert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-
 
 import org.junit.Ignore;
 //import org.junit.jupiter.api.Test;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
-import com.fasterxml.jackson.annotation.JacksonInject;
-import com.tissue_cell.dao.LoginDAO;
-import com.tissue_cell.dao.LoginDAOImpl;
 import com.tissue_cell.dto.UserDTO;
 import com.tissue_cell.service.LoginService;
-import com.tissue_cell.service.LoginServiceImpl;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations={"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"})
@@ -31,7 +22,6 @@ public class SignUpTest {
 	private LoginService login;
 	
 	@Test
-//	@Ignore
 	public void 회원가입() {
 		UserDTO user = new UserDTO();
 		user.setName("test");
@@ -45,27 +35,6 @@ public class SignUpTest {
 			System.out.println("중복아이디 없음 저장 성공");
 			login.InsertAccount(user);
 		}
-		
-		
-		
-	}
-	@Ignore
-	@Test
-	public void 중복회원_체크() {
-		UserDTO user1 = new UserDTO();
-		user1.setName("test");
-		user1.setEmail("test@gmail.com");
-		user1.setId("user");
-		user1.setPassword("1234");
-		
-		UserDTO user2 = new UserDTO();
-		user2.setName("test2");
-		user2.setEmail("test2@gmail.com");
-		user2.setId("user");
-		user2.setPassword("12345");
-		login.InsertAccount(user1);
-		IllegalStateException e = assertThrows(IllegalStateException.class, () -> login.InsertAccount(user2));
-		System.out.println(e.getMessage());
 	}
 	
 }
