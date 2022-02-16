f45df198efe751c0099793af154d484b16b35489
k971119 <95323665+HanyeolKo@users.noreply.github.com>
2022-02-16 10:06:03 +0900

    fix : 모델 패키지 삭제

--- src/main/java/com/tissue_cell/model/OAuthLogin.java
+++
@@ -1,7 +0,0 @@
-package com.tissue_cell.model;
-
-public interface OAuthLogin {
-	public String getAccessToken(String code) throws Exception;
-	
-	public String getUserEmail(String accessToken) throws Exception;
-}
