fee6542066c7a0d447304d7ecf4aae6cbd9d2051
k971119 <95323665+HanyeolKo@users.noreply.github.com>
2022-02-23 10:45:43 +0900

    fix : 사용자 DTO 등록시간 추가 이메일 삭제

--- src/main/java/com/tissue_cell/dto/UserDTO.java
+++ src/main/java/com/tissue_cell/dto/UserDTO.java
@@ -7,7 +7,7 @@ import lombok.Data;
 public class UserDTO {
 	private String id;
 	private String password;
+	private String datetime;
 	private String name;
-	private String email;
 	private String token;
 }
